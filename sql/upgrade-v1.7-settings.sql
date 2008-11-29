f7f055ad6b4c2925c64108f302dd9cf566b241b5
Aredridel <aredridel@nbtsc.org>
2008-11-29 13:39:30 -0700

    - change default settings

--- sql/upgrade-v1.7-settings.sql
+++ sql/upgrade-v1.7-settings.sql
@@ -1,8 +1,11 @@
+# Upgrade 1.7 - Settings
+# --------------------------------------------------------
+
 CREATE TABLE `settings` (
   `name` varchar(100) NOT NULL,
   `value` varchar(255) default NULL,
   PRIMARY KEY  (`name`)
  );
-INSERT INTO settings VALUES('default_title', 'Test Rapidweb');
-INSERT INTO settings VALUES('default_meta_keywords', 'Rapidweb, Olean, Content Management');
-INSERT INTO settings VALUES('default_meta_description', 'An easy content management system'); 
+INSERT INTO settings VALUES('default_title', 'Blumenthals.com Rapidweb Website');
+INSERT INTO settings VALUES('default_meta_keywords', '');
+INSERT INTO settings VALUES('default_meta_description', '');
