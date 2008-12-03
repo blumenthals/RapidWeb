957f6bd6f3731b082abd93d76516cf581e12b9b5
Aredridel <aredridel@nbtsc.org>
2008-12-03 16:15:04 -0700

    - add missing variables

--- sql/base.sql
+++ sql/base.sql
@@ -220,3 +220,8 @@ REPLACE INTO `wiki` (`pagename`, `version`, `flags`, `author`, `lastmodified`, `
 
 REPLACE INTO `wiki` (`pagename`, `version`, `flags`, `author`, `lastmodified`, `created`, `content`, `refs`, `title`, `keywords`, `meta`, `variables`, `template`) VALUES
 ('SiteMap', 1, 0, 'admin', 1227977883, 1227977883, '*[Home]\n*[Services]\n*[Products]\n*[Contact Us]\n*[Privacy Notice]\n*[Links]\n*[Search|FindPage]\n\n*Custom Error Pages:\n**[404-FileNotFound]\n**[403-Restricted]\n**[500-ServerError]\n**[401-AuthorizationRequired]\n\n*[Blumenthals  Olean NY Web Hosting Support options|BlumenthalsSupport]', 'a:0:{}', '', '', '', '', NULL);
+
+# Upgrade 1.17 - Missing variables on archive table
+---------------------------------------------------------
+alter table archive add variables text;
+alter table archive add template text;
