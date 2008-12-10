6a13216b62800d173fd385120416cebfa588f84f
Aredridel <aredridel@nbtsc.org>
2008-12-10 16:07:19 -0700

    - don't activate by default - Correct SQL files

--- sql/upgrade-v1.17-missing-sql.sql
+++ sql/upgrade-v1.17-missing-sql.sql
@@ -1,4 +1,4 @@
 # Upgrade 1.17 - Missing variables on archive table
-#---------------------------------------------------------
+# ---------------------------------------------------------
 alter table archive add variables text;
 alter table archive add template text;
