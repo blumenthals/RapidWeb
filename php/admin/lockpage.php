1f7da2e60c3238f54f83d6fddfdfac7bddf26fed
Aria Stewart <aria@blumenthals.com>
2010-04-08 17:58:50 -0400

    - fix lock again

--- php/admin/lockpage.php
+++ php/admin/lockpage.php
@@ -1,8 +1,8 @@
 <?php
    /* $Id: lockpage.php,v 1.1 2000/11/08 15:30:16 ahollosi Exp $ */
 
-   $pagename = rawurldecode($page);
 
+   $pagename = rawurldecode(isset($_REQUEST['lock']) ? $_REQUEST['lock'] : $_REQUEST['unlock']);
    $pagehash = RetrievePage($dbi, $pagename, $WikiPageStore);
    if (! is_array($pagehash))
       ExitWiki("Unknown page '".htmlspecialchars($pagename)."'\n");
