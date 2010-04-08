64ba4cbc804cdfaa8a1a03d2b1e381172d5d4cf8
Aria Stewart <aria@blumenthals.com>
2010-04-08 17:17:06 -0400

    - simplify lock/unlock

--- php/admin/lockpage.php
+++ php/admin/lockpage.php
@@ -1,22 +1,22 @@
-<!-- $Id: lockpage.php,v 1.1 2000/11/08 15:30:16 ahollosi Exp $ -->
 <?php
-   if(isset($lock)) $page = $lock;
-   elseif(isset($unlock)) $page = $unlock;
+   /* $Id: lockpage.php,v 1.1 2000/11/08 15:30:16 ahollosi Exp $ */
 
-   $argv[0] = $page;  // necessary for displaying the page afterwards
    $pagename = rawurldecode($page);
 
    $pagehash = RetrievePage($dbi, $pagename, $WikiPageStore);
    if (! is_array($pagehash))
       ExitWiki("Unknown page '".htmlspecialchars($pagename)."'\n");
-
-   if (isset($lock)) {
+   if (isset($_REQUEST['lock'])) {
       $pagehash['flags'] |= FLAG_PAGE_LOCKED;
       InsertPage($dbi, $pagename, $pagehash);
       // echo htmlspecialchars($page) . " locked\n";
-   } elseif(isset($unlock)) {
+      header("Location: admin.php?$page");
+      exit();
+   } elseif(isset($_REQUEST['unlock'])) {
       $pagehash['flags'] &= ~FLAG_PAGE_LOCKED;
       InsertPage($dbi, $pagename, $pagehash);
       // echo htmlspecialchars($page) . " unlocked\n";
+      header("Location: admin.php?$page");
+      exit();
    }
 ?>
