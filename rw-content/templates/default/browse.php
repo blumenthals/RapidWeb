e251b03b93191ed4bc99ab71b38c323b84d93b82
Aria Stewart <aria@blumenthals.com>
2010-08-12 20:43:44 -0400

    - use main toolbar file

--- rw-content/templates/default/browse.php
+++ rw-content/templates/default/browse.php
@@ -25,21 +25,7 @@
           <td colspan="2" valign="top" background="<?php bloginfo('template_directory'); ?>/images/centerbg.gif" class="repeatx"><table width="100%" border="0" cellspacing="0" cellpadding="0">
               <tr> 
                 <td height="76" align="right" valign="middle">
-                  <?php if(defined('WIKI_ADMIN')): ?>
-                  <div class='controls'>
-                    <table width="100%" border="0" cellspacing="0" cellpadding="0">
-                      <tr>
-                        <td>
-                          <?php include 'admin/toolbar-left.php' ?>
-                        </td>
-                        <td align="right">
-                          <?php include 'admin/toolbar-right.php' ?>
-                        </td>
-                      </tr>
-                    </table>
-                    <hr noshade>
-                  </div>
-                  <?php endif; ?>
+		  <?php include 'admin/toolbar.php' ?>
                 </td>
               </tr>
             </table>
