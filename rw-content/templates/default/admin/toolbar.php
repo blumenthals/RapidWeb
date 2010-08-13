3c57d0c0e1a45fcfd50907104227b64c2a7df64c
Aria Stewart <aria@blumenthals.com>
2010-08-12 20:34:31 -0400

    - add main toolbar that includes both halves

---
+++ rw-content/templates/default/admin/toolbar.php
@@ -0,0 +1,15 @@
+<?php if(defined('WIKI_ADMIN')): ?>
+<div class='controls'>
+  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+      <td>
+        <?php include 'admin/toolbar-left.php' ?>
+      </td>
+      <td align="right">
+        <?php include 'admin/toolbar-right.php' ?>
+      </td>
+    </tr>
+  </table>
+  <hr noshade>
+</div>
+<?php endif; ?>
