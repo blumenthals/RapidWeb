3314a8467c76eda98a7b54a9e7cd09e5d3efdcde
Aria Stewart <aria@blumenthals.com>
2010-08-15 16:00:32 -0400

    - use floats on divs rather than a table in toolbar

--- rw-content/templates/default/admin/toolbar.php
+++ rw-content/templates/default/admin/toolbar.php
@@ -1,15 +1,6 @@
 <?php if(defined('WIKI_ADMIN')): ?>
 <div class='controls'>
-  <table width="100%" border="0" cellspacing="0" cellpadding="0">
-    <tr>
-      <td>
-        <?php include 'toolbar-left.php' ?>
-      </td>
-      <td align="right">
-        <?php include 'toolbar-right.php' ?>
-      </td>
-    </tr>
-  </table>
-  <hr noshade>
+	<div style='float: left'> <?php include 'toolbar-left.php' ?> </div>
+	<div style='float: right'> <?php include 'toolbar-right.php' ?> </div>
 </div>
 <?php endif; ?>
