281a4a26b296cdc38f8f764cf7bf64992e4697a7
Aria Stewart <aria@blumenthals.com>
2010-11-02 15:26:23 -0400

    Make HTTP Auth work in CGI mode too.

--- rw-admin/require-authentication.php
+++ rw-admin/require-authentication.php
@@ -1,7 +1,13 @@
 <?php 
-   if (!$_SERVER['PHP_AUTH_USER'] or !isset($USERS[$_SERVER['PHP_AUTH_USER']]) or  $USERS[$_SERVER['PHP_AUTH_USER']] != $_SERVER["PHP_AUTH_PW"]) {
-      Header("WWW-Authenticate: Basic realm=\"RapidWeb Admin\"");
-      Header("HTTP/1.0 401 Unauthorized");
-      die(gettext("You entered an invalid login or password."));
-   }
+
+	if(!isset($_SERVER['PHP_AUTH_USER]']) && isset($_SERVER['HTTP_AUTHORIZATION'])) {
+		list($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) = explode(':' , base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6)));
+	}
+
+
+	if (!$_SERVER['PHP_AUTH_USER'] or !isset($USERS[$_SERVER['PHP_AUTH_USER']]) or  $USERS[$_SERVER['PHP_AUTH_USER']] != $_SERVER["PHP_AUTH_PW"]) {
+		Header("WWW-Authenticate: Basic realm=\"RapidWeb Admin\"");
+		Header("HTTP/1.0 401 Unauthorized");
+		die(gettext("You entered an invalid login or password."));
+	}
 ?>
