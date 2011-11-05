f1fef3996af6f7f612c96cb08f97af83bf255860
Aria Stewart <aria@blumenthals.com>
2011-11-05 00:59:13 -0400

    Move editor code into plugin

--- rw-content/plugins/rw-gallery/plugin.php
+++ rw-content/plugins/rw-gallery/plugin.php
@@ -44,36 +44,33 @@ class RWGallery extends RWPlugin {
 
     public function the_editor_content($view) {
     ?>
-      <div id='rwgallery_editor' class='rapidweb-editor'>
-        <section class='details-box'>
-          <h3 class='details-box-show'>
-            <img src="<?php bloginfo('template_directory'); ?>/../default/admin/arrow-down.gif" align="absmiddle"> Instructions
-          </h3>
-          <h3 class='details-box-hide'>
-            <img src="<?php bloginfo('template_directory'); ?>/../default/admin/arrow-over.gif" align="absmiddle"/> Instructions
-          </h3>
-          <div class='details'>
-            <p>Drag to re-arrange.</p>
-            <p>Click the X on a picture to remove it.</p>
-            <p>JPEG, GIF and PNG files all work.</p>
-            <p>Any picture over 1000x1000 pixels is resized.</p>
-            <p>Be patient uploading. Sending full-size pictures can be slow!</p>
-          </div>
-        </section>
-        <div class='gallery-tile upload-tile' id='upload-tile'>
-          <iframe style='display: none' id='upload_target' name='upload_target'></iframe>
-          <form action='<?php echo $view->getScriptURL(); ?>' target='upload_target' method='post' enctype='multipart/form-data'>
-            <div class='file-upload'>
-              Upload a file
-              <input type='file' name='img'>
-              <input type='hidden' name='pagename' value='<?php echo $view->page->pagename; ?>'>
-              <input type='hidden' name='command' value='upload_image_ajax'>
-            </div>
-            <div class='spinner'><img src='<?php echo "{$this->baseURL}loader.gif" ?>'></div>
-            <div class='error'></div>
-          </form>
+      <section class='details-box'>
+        <h3 class='details-box-show'>
+          <img src="<?php bloginfo('template_directory'); ?>/../default/admin/arrow-down.gif" align="absmiddle"> Instructions
+        </h3>
+        <h3 class='details-box-hide'>
+          <img src="<?php bloginfo('template_directory'); ?>/../default/admin/arrow-over.gif" align="absmiddle"/> Instructions
+        </h3>
+        <div class='details'>
+          <p>Drag to re-arrange.</p>
+          <p>Click the X on a picture to remove it.</p>
+          <p>JPEG, GIF and PNG files all work.</p>
+          <p>Any picture over 1000x1000 pixels is resized.</p>
+          <p>Be patient uploading. Sending full-size pictures can be slow!</p>
         </div>
-
+      </section>
+      <div class='gallery-tile upload-tile' id='upload-tile'>
+        <iframe style='display: none' id='upload_target' name='upload_target'></iframe>
+        <form action='<?php echo $view->getScriptURL(); ?>' target='upload_target' method='post' enctype='multipart/form-data'>
+          <div class='file-upload'>
+            Upload a file
+            <input type='file' name='img'>
+            <input type='hidden' name='pagename' value='<?php echo $view->page->pagename; ?>'>
+            <input type='hidden' name='command' value='upload_image_ajax'>
+          </div>
+          <div class='spinner'><img src='<?php echo "{$this->baseURL}loader.gif" ?>'></div>
+          <div class='error'></div>
+        </form>
       </div>
     <?php
     }
