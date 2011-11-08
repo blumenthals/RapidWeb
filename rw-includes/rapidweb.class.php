ff6d36e8a16b7584af4565ce545d62b091c87694
Aria Stewart <aredridel@nbtsc.org>
2011-11-07 23:22:24 -0500

    Aria, do you even check things before you commit?

--- rw-includes/rapidweb.class.php
+++ rw-includes/rapidweb.class.php
@@ -26,7 +26,7 @@ class RapidWeb extends EventEmitter {
 
         /* Set up variables */
         $this->documentRoot = $real;
-        $this->globalURL = "$url/rw-global/";
+        $this->globalURL = "{$url}rw-global/";
         $this->rootURL = $url;
 
         /* Register basic page type plugin */
