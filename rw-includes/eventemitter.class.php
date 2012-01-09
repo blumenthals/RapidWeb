729da958a302d7d73ffddd3b6c15f712f6ca3f9f
Aria Stewart <aria@blumenthals.com>
2012-01-08 21:41:45 -0500

    Add 'filter' event function

--- rw-includes/eventemitter.class.php
+++ rw-includes/eventemitter.class.php
@@ -16,5 +16,15 @@ class EventEmitter {
             call_user_func_array($callback, $args);
         }
     }
+
+    public function filter($event) {
+        $args = func_get_args();
+        array_shift($args);
+        if(!isset($this->callbacks[$event])) return;
+        foreach($this->callbacks[$event] as $callback) {
+            $args[0] = call_user_func_array($callback, $args);
+        }
+        return $args[0];
+    }
 }
 
