a6b11fddf595e90971eba200532296614e1c2dc4
Aria Stewart <aria@blumenthals.com>
2012-01-29 12:05:24 -0500

    Fix PDO use of Settings when creating new pages

--- rw-includes/mysql.php
+++ rw-includes/mysql.php
@@ -158,7 +158,7 @@ function RetrievePage(PDO $dbc, $pagename) {
         'lastmodified' => time(),
         'author' => '',
         'plugins' => new StdClass(),
-        'settings' => RetrieveSettings()
+        'settings' => RetrieveSettings($dbc)
     );
 }
 
