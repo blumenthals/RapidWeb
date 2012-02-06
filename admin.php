8f30dbca345294d657ebc776a8e58d448b966567
Aria Stewart <aria@blumenthals.com>
2012-02-06 00:12:05 -0500

    Reformat admin.php

--- admin.php
+++ admin.php
@@ -17,52 +17,51 @@ if(preg_match('!^text/json(; .*)?$!', $_SERVER['CONTENT_TYPE'])) {
     exit();
 }
 
-   if(isset($_REQUEST['lock']) || isset($_REQUEST['unlock'])) {
-      include ('rw-admin/lockpage.php');
-      ExitWiki('');
-   } elseif (isset($_REQUEST['zip'])) {
-      $zip = $_REQUEST['zip'];
-      include ('rw-includes/ziplib.php');
-      include ('rw-admin/zip.php');
-      ExitWiki('');
-   } elseif (isset($_REQUEST['dumpserial'])) {
-      $dumpserial = $_REQUEST['dumpserial'];
-      include ('rw-admin/dumpserial.php');
-   } elseif (isset($_REQUEST['loadserial'])) {
-      $loadserial = $_REQUEST['loadserial'];
-      include ('rw-admin/loadserial.php');
-   } elseif (isset($_REQUEST['remove'])) {
-      if (get_magic_quotes_gpc()) {
-         $remove = stripslashes($_REQUEST['remove']);
-      } else {
-         $remove = $_REQUEST['remove'];
-      }
-      if (function_exists('RemovePage')) {
+if(isset($_REQUEST['lock']) || isset($_REQUEST['unlock'])) {
+    include ('rw-admin/lockpage.php');
+    ExitWiki('');
+} elseif (isset($_REQUEST['zip'])) {
+    $zip = $_REQUEST['zip'];
+    include ('rw-includes/ziplib.php');
+    include ('rw-admin/zip.php');
+    ExitWiki('');
+} elseif (isset($_REQUEST['dumpserial'])) {
+    $dumpserial = $_REQUEST['dumpserial'];
+    include ('rw-admin/dumpserial.php');
+} elseif (isset($_REQUEST['loadserial'])) {
+    $loadserial = $_REQUEST['loadserial'];
+    include ('rw-admin/loadserial.php');
+} elseif (isset($_REQUEST['remove'])) {
+    if (get_magic_quotes_gpc()) {
+        $remove = stripslashes($_REQUEST['remove']);
+    } else {
+        $remove = $_REQUEST['remove'];
+    }
+    if (function_exists('RemovePage')) {
         $html .= sprintf(gettext ("You are about to remove '%s' permanently!"), 
-          htmlspecialchars($remove));
+            htmlspecialchars($remove));
         $html .= "\n<P>";
         $url = rawurlencode($remove);
         $html .= sprintf(gettext ("Click %shere%s to remove the page now."),
-          "<A HREF=\"$ScriptUrl?removeok=$url\">", "</A>");
+            "<A HREF=\"$ScriptUrl?removeok=$url\">", "</A>");
         $html .= "\n<P>";
         $html .= gettext ("Otherwise press the \"Back\" button of your browser.");
-      } else {
-         $html = gettext ("Function not yet implemented.");
-      }
-      GeneratePage('MESSAGE', $html, gettext ("Remove page"), 0);
-      ExitWiki('');
-   } elseif (isset($_REQUEST['removeok'])) {
-      if (get_magic_quotes_gpc()) {
+    } else {
+        $html = gettext ("Function not yet implemented.");
+    }
+    GeneratePage('MESSAGE', $html, gettext ("Remove page"), 0);
+    ExitWiki('');
+} elseif (isset($_REQUEST['removeok'])) {
+    if (get_magic_quotes_gpc()) {
         $removeok = stripslashes($_REQUEST['removeok']);
-      } else {
+    } else {
         $removeok = $_REQUEST['removeok'];
-      }
-      RemovePage($dbc, $removeok);
-      $html = sprintf(gettext ("Removed page '%s' succesfully."),
+    }
+    RemovePage($dbc, $removeok);
+    $html = sprintf(gettext ("Removed page '%s' succesfully."),
         htmlspecialchars($removeok));
-      GeneratePage('MESSAGE', $html, gettext ("Remove page"), 0);
-      ExitWiki('');
-   }
+    GeneratePage('MESSAGE', $html, gettext ("Remove page"), 0);
+    ExitWiki('');
+}
 
-   include('index.php');
-?>
+include('index.php');
