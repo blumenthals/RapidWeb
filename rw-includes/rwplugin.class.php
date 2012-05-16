ea0f27dc506c95d1785a0250cbec657c02fe3bad
Aria Stewart <aredridel@nbtsc.org>
2012-05-15 23:19:09 -0400

    Add "loadJavascript" to RWPlugin

--- rw-includes/rwplugin.class.php
+++ rw-includes/rwplugin.class.php
@@ -15,4 +15,13 @@ class RWPlugin {
     public static function initialize($rapidweb) {
         new static($rapidweb);
     }
+
+    public function loadJavascript($script) {
+        /// @todo: Make this look up resources from other bundles
+        echo "<script src='".$this->assetURL($script)."'></script>";
+    }
+
+    public function assetURL($path) {
+        return $this->baseURL . $path;
+    }
 }
