60a592fffee3874227beeb3e788fb86f7c15a741
Aria Stewart <aria@blumenthals.com>
2012-05-19 12:50:39 -0400

    Improve setup.php

--- rw-admin/setup.php
+++ rw-admin/setup.php
@@ -1,6 +1,11 @@
 <?php
 
+echo "Compiling LESS files...";
+flush();
 require_once __DIR__."/../rw-global/lessphp/lessc.inc.php";
 
-chdir("../rw-global");
-lessc::ccompile(__DIR__."/../rw-global/less/rapidweb.less", __DIR__."/../rw-global/css/rapidweb.css");
+$lessc = new lessc(__DIR__."/../rw-global/less/rapidweb.less");
+$out = $lessc->parse();
+file_put_contents(__DIR__."/../rw-global/css/rapidweb.css", $out);
+
+echo "Done";
