d3f74c264ada40a6034e0781e5dd706add9b36cd
Aria Stewart <aredridel@nbtsc.org>
2012-05-19 12:27:40 -0400

    chdir for lessphp

--- rw-admin/setup.php
+++ rw-admin/setup.php
@@ -2,4 +2,5 @@
 
 require_once __DIR__."/../rw-global/lessphp/lessc.inc.php";
 
+chdir("../rw-global");
 lessc::ccompile(__DIR__."/../rw-global/less/rapidweb.less", __DIR__."/../rw-global/css/rapidweb.css");
