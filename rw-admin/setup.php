39348ce2c48d2c0cf7a95f56d840af85ca846d1d
Aria Stewart <aredridel@nbtsc.org>
2012-05-31 07:41:03 -0400

    Fix setup.php to compile LESS again, even if it does temporarily need recess

--- rw-admin/setup.php
+++ rw-admin/setup.php
@@ -1,18 +1,20 @@
 <?php
 
-/// FIXME
-/*
+header("Content-Type: text/plain");
+
 echo "Compiling LESS files...";
 flush();
+system("env PATH=\$PATH:/usr/local/bin make -C ".__DIR__."/../rw-global 2>&1");
+/** FIXME
 require_once __DIR__."/../rw-global/lessphp/lessc.inc.php";
 
 $lessc = new lessc(__DIR__."/../rw-global/less/rapidweb.less");
 $out = $lessc->parse();
 file_put_contents(__DIR__."/../rw-global/css/rapidweb.css", $out);
+ */
 
 echo "Done\n";
 flush();
- */
 echo "Updating schema...";
 
 flush();
