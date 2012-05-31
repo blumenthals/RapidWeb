8ada6c3ab9d67c9576f5f7d26a0a38ae4b08d822
Aria Stewart <aredridel@nbtsc.org>
2012-05-31 07:29:24 -0400

    Update autoloader setup

--- rw-includes/config.php
+++ rw-includes/config.php
@@ -102,4 +102,4 @@ set_include_path(get_include_path().PATH_SEPARATOR.dirname(__FILE__));
 
 spl_autoload_extensions('.class.php');
 
-spl_autoload_register();
+spl_autoload_register('spl_autoload');
